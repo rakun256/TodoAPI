e09148976cb6bfaceb82812fc7c5232b256f9c3c
Emre Uslu (Dogus Teknoloji) <Emre.Uslu@d-teknoloji.com.tr>
2025-07-09 11:06:41 +0300

    #06 GetTodoById endpointi eklendi

---
+++ Todo.Application/Features/Todos/Queries/GetTodoById/GetTodoByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Todo.Application.Features.Todos.Queries.GetAllTodos;
+
+namespace Todo.Application.Features.Todos.Queries.GetTodoById
+{
+    public class GetTodoByIdQuery : IRequest<TodoDto>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Todo.Application/Features/Todos/Queries/GetTodoById/GetTodoByIdQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Todo.Application.Features.Todos.Queries.GetTodoById
+{
+    public class GetTodoByIdQueryValidator : AbstractValidator<GetTodoByIdQuery>
+    {
+        public GetTodoByIdQueryValidator()
+        {
+            RuleFor(x => x.Id).GreaterThan(0);
+        }
+    }
+}
