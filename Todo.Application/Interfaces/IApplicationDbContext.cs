21f870ca744f4333bd605c502e80728f27cb6c84
Emre Uslu (Dogus Teknoloji) <Emre.Uslu@d-teknoloji.com.tr>
2025-07-09 10:24:44 +0300

    #02 ApplicationDbContext oluşturuldu ve TodoItem eklendi

---
+++ Todo.Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Todo.Domain.Entities;
+
+namespace Todo.Application.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<TodoItem> TodoItems { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
