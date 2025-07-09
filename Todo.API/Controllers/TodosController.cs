ca2a87ec2b04f189cb220276d8c1865f1dd684e9
Emre Uslu (Dogus Teknoloji) <Emre.Uslu@d-teknoloji.com.tr>
2025-07-09 10:51:04 +0300

    #04 CreateTodo controller ve dependency injection tamamlandı

---
+++ Todo.API/Controllers/TodosController.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Todo.Application.Features.Todos.Commands.CreateTodo;
+
+namespace Todo.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TodosController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TodosController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateTodoCommand command)
+        {
+            var id = await _mediator.Send(command);
+            return CreatedAtAction(nameof(GetById), new { id }, command);
+        }
+
+        // Bu placeholder, sonradan GetById endpointi geldiğinde çalışır hale gelecek
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok(); // Geçici
+        }
+    }
+}
--- Todo.API/Program.cs
+++ Todo.API/Program.cs
@@ -1,23 +1,28 @@
+using FluentValidation;
+using MediatR;
+using System.Reflection;
+using Todo.Application;
+using Todo.Infrastructure;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Application & Infrastructure katmanlarýný ekle
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
